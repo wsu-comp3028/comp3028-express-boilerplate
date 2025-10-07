9481e49ca99a64c5031421169dad72803d4a0f3b
Rhys Tague <54823859+drtaguewsu@users.noreply.github.com>
2025-10-07 19:26:55 +1100

    Initialize DB and update server start logic
    Added database initialization and middleware to app.js. Modified server start logic to conditionally run based on environment.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import debug from 'debug';
 import * as server from './config/server.js';
 import { homeRouter } from './routes/home.js';
+import {initializeDB, dbMiddleware} from './config/db.js'
 
 // Setup debug module to spit out all messages
 // Do `npn start` to see the debug messages
@@ -13,15 +14,25 @@ server.setup(app)
 
 // Register any middleware here
 
+// DB connection
+initializeDB();
+app.use(dbMiddleware);
+
 // Register routers here
 app.use('/', homeRouter);
 
 
 
 // ####################################### No need to modify below this line #######################################
-// Start the server
-server.errorHandling(app);
-app.listen(server.port, () => {
-  console.log(`Example app listening on port http://127.0.0.1:${server.port}`);
-  debug('testing');
-});
+export let runningServer;
+// Only start server if not in test mode
+// This allows supertest to run without the server already listening
+// run tests with "NODE_ENV=test node --test"
+if(process.env.NODE_ENV === 'test') {
+  codeTrace('Running in test mode - not starting server, just exporting app');
+} else {
+  // Start the server
+  runningServer = app.listen(server.port, () => {
+    console.log(`Example app listening on port http://127.0.0.1:${server.port}`);
+  });
+}
